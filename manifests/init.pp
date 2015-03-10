d1f4c5797c240a8c604c5799c17bc3cacce91acc
Richard Delph <richard.delph@core.je>
2015-03-10 04:21:37 +0000

    Add default/conditional for home directory

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,13 @@ define netrc::foruser($root_home_directory="/home",$user, $machine_user_password
 	$filename = ".netrc"
 	$prefixes = ['machine ','	login ','	password ']
 	
-	file { "$root_home_directory/$user/$filename":
+	if $root_home_directory != "/home" {
+		$netrc_path = $root_home_directory
+	} else {
+		$netrc_path = "$root_home_directory/$user"
+	}
+
+	file { "$netrc_path/$filename":
 		ensure => present,
 		content => template('netrc/netrc.erb'),
 		mode => '0600',
